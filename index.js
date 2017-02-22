23cebc9dca772efe5cf525fe7b6401b7fde91c19
jozanza <hello@jsavary.com>
2017-02-22 09:52:26 -0500

    replaced local modules with external package

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const parse = require('./parse');
-const _ = require('./utils');
+const jsonify = require('jsonify-css');
 
 module.exports = function loader(content) {
   if (this.cacheable) this.cacheable();
-  const styles = Object.assign({ raw: content }, parse(this.context)(content));
+  const styles = Object.assign({ raw: content }, jsonify({ root: this.context })(content));
   const _exports = Object.keys(styles).map(k => `export const ${k} = ${JSON.stringify(styles[k])}`);
-  const _default = JSON.stringify(styles.rule.concat(styles.media).reduce((a, b) => _.merge(a, b), {}));
+  const _default = JSON.stringify(styles.rule.concat(styles.media).reduce((a, b) => Object.assign({}, a, b), {}));
   return _exports.join('\n') + '\nexport default ' + _default;
 };
--- utils.js
+++
@@ -1,3 +0,0 @@
-const merge = exports.merge = (a, b) => Object.assign({}, a, b);
-
-const camelize = exports.camelize = a => a.replace(/-([a-z])/g, b => b[1].toUpperCase());
